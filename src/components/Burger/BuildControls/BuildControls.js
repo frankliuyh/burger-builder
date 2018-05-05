1d442404a7ad344308434921eb2b956fcfbd19ca
Frank Liu <437730547@qq.com>
2018-05-05 02:53:06 +0000

    add more-button function

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,9 +9,9 @@ const controls = [
     {label: 'Meat', type: 'meat'},
 ];
 
-const buildControls = () => (
+const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+        {controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} added={() => props.ingredientAdded(ctrl.type)} />)}
     </div>
 );
 
