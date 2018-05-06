a4d8820785a3756370318b81e623662f32932661
Frank Liu <437730547@qq.com>
2018-05-06 03:10:43 +0000

    add OrderSummary animation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,12 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
+    }
+    
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
     }
     
     updatePurchaseState = (ingredients) => {
@@ -58,12 +63,12 @@ class BurgerBuilder extends Component {
         }
         return (
             <React.Fragment>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler} 
-                                disabled={disabledInfo} price={this.state.totalPrice} purchasable={this.state.purchasable} />
+                                disabled={disabledInfo} price={this.state.totalPrice} purchasable={this.state.purchasable} ordered={this.purchaseHandler} />
             </React.Fragment>
         );
     }
