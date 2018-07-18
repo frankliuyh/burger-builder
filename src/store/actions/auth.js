aa36a13c1f06d5f25271c30ea58fe6c6496b2530
frankliuyh <frankliuyh@qq.com>
2018-07-18 23:07:14 +0800

    use token to protect some resources

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -33,7 +33,7 @@ export const checkAuthTimeout = (expirationTime) => {
 	return dispatch => {
 		setTimeout(() => {
 			dispatch(logout());
-		}, expirationTime * 1000)
+		}, expirationTime * 1000);
 	};
 };
 
@@ -53,7 +53,7 @@ export const auth = (email, password, isSignup) => {
 			.then(response => {
 				console.log(response);
 				dispatch(authSuccess(response.data.idToken, response.data.localId));
-				dispatch(checkAuthTimeout(response.data.expriresIn));
+				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
 			.catch(err => {
 				console.log(err);
